22d4a4af69461a33aa4213cb6dc2e7b1feb0039f
Cranzai <cranzai.gaming@gmail.com>
2021-12-02 10:30:44 +0100

    Upload day 1
    Creation of I/O file handler was of course the most annoying.

---
+++ day1Prog.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define maxFileName 10
+
+int *readIntList(const char *fileName, int *numLines);
+
+void main()
+{
+  int i;
+  int depthsLen=0;
+  int incrs = 0;
+  int decrs = 0;
+  char fileName[maxFileName];
+
+  scanf("%s", fileName);
+  int *depths = readIntList(fileName, &depthsLen);
+
+  for (i = 1; i < depthsLen; ++i)
+  {
+    if (depths[i] > depths[i-1])
+    {
+      incrs = incrs + 1;
+    }
+  } 
+  printf("Part A) increases %d", incrs);
+
+  int depthsThree[depthsLen-1];
+  for (i = 0; i < depthsLen-2; ++i)
+  {
+    depthsThree[i] = depths[i] + depths[i+1] + depths[i+2];    
+  }
+ 
+  incrs=0;
+  decrs=0;
+ 
+  for (i = 1; i < depthsLen-2; ++i)
+  {
+    if (depthsThree[i] > depthsThree[i-1])
+    {
+      incrs = incrs + 1;
+    }
+  } 
+  printf("Part B) increases %d", incrs);
+}
+
+int *readIntList(const char *fileName, int *numLines)
+{
+  int *intList; 
+  int i;
+  char dum;
+
+  FILE* fileIn = fopen(fileName, "r");
+  if (fileIn == NULL)
+  {
+    printf("Could not open file %s \n", fileName);
+    exit(0);
+  }
+
+  for (dum = getc(fileIn); dum != EOF; dum = getc(fileIn))
+    if (dum == '\n')
+      *numLines = *numLines + 1;
+
+  rewind(fileIn);
+
+  intList = (int*)malloc(*numLines * sizeof(int));
+  if (intList == NULL)
+  {
+    printf("Could not allocate intList \n");
+    exit(1);
+  }
+
+  for (i = 0; i < *numLines; ++i)
+  {
+    fscanf(fileIn, "%d", &intList[i]);
+  }
+
+  fclose(fileIn);
+  return intList;
+}
