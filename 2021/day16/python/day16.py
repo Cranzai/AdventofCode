532caafeafdc6fc26e141f915bd32f0722c6cb28
Cranzai <cranzai.gaming@gmail.com>
2021-12-16 22:28:01 +0100

    Day 16 Final
    Packets are parsed and operators/values to operate on are printed.
    
    Got fed up with trying to solve this programmatically. Wanted to get the star so did it manually.

--- 2021/day16/python/day16.py
+++ 2021/day16/python/day16.py
@@ -1,7 +1,8 @@
 import sys
 from collections import deque
+import copy
 
-class packet:
+class BITS:
     def __init__(self, input_file):
         with open(input_file, "r") as file:
             self.message = file.readline().rstrip("\n")
@@ -26,38 +27,88 @@ class packet:
         }
 
         self.versionSum = 0
+        self.rawValues = []
+        self.values = deque()
+        self.operators = deque()
 
     def decode(self):
-        print('decode')
         self.binary = deque()
         for char in list(self.message):
             for bit in self.hex_bin[char]:
                 self.binary.append(bit)
-        
-        self.packet(self.binary, 0, 0)
 
-    def packet(self, block, value, ops):
-        print(block)
+        self.packet(self.binary)
+
+        values = [value for value in self.rawValues]
+        self.values.append(values)
+        self.values.popleft()
+
+        return
+
+    def operate(self, operator, value_set):
+        if operator == 0:
+            return sum(value_set)
+
+        if operator == 1:
+            product = 1
+            for val in value_set:
+                product = product * val
+
+            return product
+
+        if operator == 2:
+            return min(value_set)
+
+        if operator == 3:
+            return max(value_set)
+
+        if operator == 5:
+            if value_set[0] > value_set[1]:
+                return 1
+            
+            return 0
+
+        if operator == 6:
+            if value_set[0] < value_set[1]:
+                return 1
+            
+            return 0
+
+        if operator == 7:
+            if value_set[0] == value_set[1]:
+                return 1
+            
+            return 0
+
+
+    def packet(self, block):
+        #only the source packet has extra 0's, hence if the deck does no longer contain 1 we are done.
+        if not '1' in block:
+            return
+
         version = int(''.join([str(block.popleft()) for i in range(3)]), 2)
         self.versionSum += version
         typeID = int(''.join([str(block.popleft()) for i in range(3)]), 2)
 
         if typeID == 4:
             value = self.literal_value(block)
+            self.rawValues.append(value)
+
         elif typeID != 4:
-            ops = self.operator(block)
+            self.operators.append(typeID)
+            values = [value for value in self.rawValues]
+            self.values.append(values)
+            self.rawValues = []
 
-        if not block:
-            print(value, ops)
-            return value, ops
+            self.operator(block)
 
-        self.packet(block, value, ops)
+        self.packet(block)
 
     def literal_value(self, block):
-        print('value')
         binary = ''
-
-        while block:  
+        count = 0
+        while block:
+            count += 1
             part = ''.join([str(block.popleft()) for i in range(5)])
             binary += part[1:]
             if part[0] == '0':
@@ -66,7 +117,6 @@ class packet:
         return int(binary, 2)
 
     def operator(self, block):
-        print('operator')
         field = 15 - 4 * int(block.popleft())
         part = deque()
 
@@ -75,22 +125,17 @@ class packet:
             for i in range(length):
                 part.append(block.popleft())
             
-            self.packet(part, 0, 0)
+            self.packet(part)
 
         if field == 11:
-            num = int(''.join([str(block.popleft()) for i in range(field)]), 2)
-            part = block
-            for i in range(num):
-                self.packet(block, 0, 0)
-
-
-
-
-
-
-
+            num = int(''.join([str(block.popleft()) for i in range(field)]), 2)            
+            self.packet(block)
 
 
 if __name__ == "__main__":
-    part_a = packet(sys.argv[1])
-    part_a.decode()
+    message = BITS(sys.argv[1])
+    message.decode()
+    print(message.versionSum)
+
+    print(message.operators)
+    print(message.values)
