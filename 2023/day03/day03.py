901939870065b3796d8038359a329844e9347031
Cranzai <cranzai.gaming@gmail.com>
2023-12-11 20:45:15 +0100

    AoC 2023 - day 3 finished

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -3,14 +3,18 @@ import re
 with open("day03.inp", "r") as file:
     schematic = [line.strip() for line in file.readlines()]
     partnumbers = []
+    gears = {}
 
     maxx = len(schematic[0])-1
     maxy = len(schematic)-1
 
+    #part A
     for y, line in enumerate(schematic):
         nums = list([(m.group(), m.start(0), m.end(0)) for m in re.finditer(r"\d+", line)])
 
         for num, start, end in nums:
+            added = False
+
             yx = []
             for x in range(start-1, end+1):
                 for dy in (-1, 0, 1):
@@ -19,7 +23,32 @@ with open("day03.inp", "r") as file:
                             yx.append((y+dy, x))
 
             for ys, xs in yx:
-                if schematic[ys][xs] != "." and not int(num) in partnumbers:
-                    partnumbers.append(int(num))
+                if schematic[ys][xs] != ".":
+                    if not added:
+                        partnumbers.append(int(num))
 
-    print(sum(partnumbers))
+                    #part B
+                    if schematic[ys][xs] == "*":
+                        key = str(ys)+","+str(xs)
+                        if key in gears.keys():
+                            gears[key].append(int(num))
+                        else:
+                            gears[key] = [int(num)]
+
+
+    print("A: ", sum(partnumbers))
+    print("B: ", sum([value[0]*value[1] for key, value in gears.items() if len(value)==2]))
+
+    exit()
+    #print B
+    for y, line in enumerate(schematic):
+        gears = [m.start(0) for m in re.finditer(r"\*", line)]
+        
+        for x in gears:
+            yx = []
+            for dx in (-1, 0, 1):
+                for dy in (-1, 0, 1):
+                    if not dy == dx == 0:
+                        yx.append(y+dy, x+dx)
+                
+
