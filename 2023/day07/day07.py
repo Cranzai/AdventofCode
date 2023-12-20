b6d178df8eee8e2c377501ffc1cf7db4975e96db
Cranzai <cranzai.gaming@gmail.com>
2023-12-20 22:10:28 +0100

    AoC 2023 - Day 07 fixed part A

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -1,7 +1,15 @@
 class CamelCards:
-    def __init__(self, game):
-        cardValue = {"2":1, "3": 2, "4": 3, "5": 4, "6": 5, "7":6, "8":7, "9":8, "T":9, "J":10, "Q":11, "K":12, "A":13}
-        self.hands = [[sorted(list(map(lambda val: cardValue[val], [*line.split()[0]])), reverse=True), int(line.split()[1])] for line in open(game, "r").readlines()]
+    def __init__(self, game, part):
+        self.part = part
+        if self.part == "A":
+            cardValue = {"2":1, "3": 2, "4": 3, "5": 4, "6": 5, "7":6, "8":7, "9":8, "T":9, "J":10, "Q":11, "K":12, "A":13}
+            self.hands = [[list(map(lambda val: cardValue[val], [*line.split()[0]])), int(line.split()[1])] for line in open(game, "r").readlines()]
+            self.__winnings()
+        
+        elif self.part == "B":
+            cardValue = {"J":1, "2":2, "3": 3, "4": 4, "5": 5, "6": 6, "7":7, "8":8, "9":9, "T":10, "Q":11, "K":12, "A":13}
+            self.hands = [[list(map(lambda val: cardValue[val], [*line.split()[0]])), int(line.split()[1])] for line in open(game, "r").readlines()]
+            self.__winnings()
 
     def __find_types(self):
         self.types = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[]}
@@ -13,41 +21,42 @@ class CamelCards:
                 elif card in count.keys():
                     count[card]+=1
 
-            counts = sorted(count.values())
-            if counts[-1] == 5:         #five of a kind
-                self.types[6].append(hand)
-            if counts[-1] == 4:         #four of a kind
-                self.types[5].append(hand)
-            if counts[-1] == 3:         
-                if counts[-2] == 2:     #full house
-                    self.types[4].append(hand)
-                else:                   #three of a kind
-                    self.types[3].append(hand)
-            if counts[-1] == 2:         
-                if counts[-2] == 2:     #two pair
-                    self.types[2].append(hand)
-                else:                   #one pair
-                    self.types[1].append(hand)
-            if counts[-1] == 1:         #high card
-                self.types[0].append(hand)
+            if self.part == "A":
+                counts = sorted(count.values())
+                if counts[-1] == 5:         #five of a kind
+                    self.types[6].append(hand)
+                if counts[-1] == 4:         #four of a kind
+                    self.types[5].append(hand)
+                if counts[-1] == 3:         
+                    if counts[-2] == 2:     #full house
+                        self.types[4].append(hand)
+                    else:                   #three of a kind
+                        self.types[3].append(hand)
+                if counts[-1] == 2:         
+                    if counts[-2] == 2:     #two pair
+                        self.types[2].append(hand)
+                    else:                   #one pair
+                        self.types[1].append(hand)
+                if counts[-1] == 1:         #high card
+                    self.types[0].append(hand)
+            elif self.part == "B":
+                exit()
 
         #i suppose this should sort the sublists properly too
         for type, vals in self.types.items():
             if len(vals)>1:
                 self.types[type] = sorted(vals, key=lambda x: (x[0][0], x[0][1], x[0][2], x[0][3], x[0][4]))
 
-    def partA(self):
+    def __winnings(self):
         if not hasattr(self, "types"):
             self.__find_types()
 
-        sum = 0
+        self.sum = 0
         rank = 1
         for hand in self.types.values():
             for _, bid in hand:
-                sum += bid*rank
+                self.sum += bid*rank
                 rank +=1
 
-        return sum
-
-day07 = CamelCards("day07.inp")
-print(day07.partA())
+print(CamelCards("day07.inp", "A").sum)
+print(CamelCards("day07.inp", "B").sum)
