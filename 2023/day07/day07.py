d2f120c9261cc9e476f49739d52bcfc9dd80409f
Cranzai <cranzai.gaming@gmail.com>
2023-12-20 22:25:54 +0100

    AoC 2023 - Day 07 Fixed

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -21,26 +21,36 @@ class CamelCards:
                 elif card in count.keys():
                     count[card]+=1
 
-            if self.part == "A":
-                counts = sorted(count.values())
-                if counts[-1] == 5:         #five of a kind
-                    self.types[6].append(hand)
-                if counts[-1] == 4:         #four of a kind
-                    self.types[5].append(hand)
-                if counts[-1] == 3:         
-                    if counts[-2] == 2:     #full house
-                        self.types[4].append(hand)
-                    else:                   #three of a kind
-                        self.types[3].append(hand)
-                if counts[-1] == 2:         
-                    if counts[-2] == 2:     #two pair
-                        self.types[2].append(hand)
-                    else:                   #one pair
-                        self.types[1].append(hand)
-                if counts[-1] == 1:         #high card
-                    self.types[0].append(hand)
-            elif self.part == "B":
-                exit()
+            if not 1 in count.keys():
+                count[1] = 0
+
+            if self.part == "B":
+                for key in count.keys():
+                    if not key == 1:
+                        print(count[1])
+                        count[key] += count[1]
+
+
+            counts = sorted(count.values())
+            if counts[-1] == 5:         #five of a kind
+                self.types[6].append(hand)
+            elif counts[-1] == 4:         #four of a kind
+                self.types[5].append(hand)
+            elif counts[-1] == 3:     
+                second = counts[-2] - count[1] if self.part == "B" else counts[-2]    
+                if second == 2:     #full house
+                    self.types[4].append(hand)
+                else:                   #three of a kind
+                    self.types[3].append(hand)
+            elif counts[-1] == 2:
+                second = counts[-2] - count[1] if self.part == "B" else counts[-2]             
+                if second == 2:     #two pair
+                    self.types[2].append(hand)
+                else:                   #one pair
+                    self.types[1].append(hand)
+            elif counts[-1] == 1:         #high card
+                self.types[0].append(hand)
+
 
         #i suppose this should sort the sublists properly too
         for type, vals in self.types.items():
